9e508743abbdf6f90ebc2ea3861d668974a0bffc
SeasonPilot <pilot1@foxmail.com>
2022-07-25 21:41:58 +0800

    style: 调整代码顺序

--- go-language-project-development-practice/behavioral-patterns/strategy-pattern/main.go
+++ go-language-project-development-practice/behavioral-patterns/strategy-pattern/main.go
@@ -4,18 +4,6 @@ type IStrategy interface {
 	Do(int, int) int
 }
 
-type Add struct{}
-
-func (*Add) Do(a, b int) int {
-	return a + b
-}
-
-type Reduce struct{}
-
-func (*Reduce) Do(a, b int) int {
-	return a - b
-}
-
 type Operator struct {
 	strategy IStrategy
 }
@@ -26,4 +14,16 @@ func (o *Operator) SetStrategy(strategy IStrategy) {
 
 func (o *Operator) Calculate(a, b int) int {
 	return o.strategy.Do(a, b)
+}
+
+type Add struct{}
+
+func (*Add) Do(a, b int) int {
+	return a + b
+}
+
+type Reduce struct{}
+
+func (*Reduce) Do(a, b int) int {
+	return a - b
 }
